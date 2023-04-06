ac48d8cbe87a163871c33a244df1e45bdd00284a
IreneDagher <irene_dagher@hotmail.com>
2023-04-06 11:14:34 +0200

    Main Page: logo added titre sign in sign up

--- app/src/main/java/com/example/winemeup/MainActivity.java
+++ app/src/main/java/com/example/winemeup/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.winemeup;
 
-import androidx.appcompat.app.AppCompatActivity;
+import
 
-import android.os.Bundle;
+import android.widget.ImageView; ...
+
+
 
 //allright
 //yes yes
 //try
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Drawable drawable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
