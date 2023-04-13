3e2d35fd2a7c5c3683d37e352a6e1c10ddbed0d6
IreneDagher <irene_dagher@hotmail.com>
2023-04-13 11:15:32 +0200

    Activity signup Activity signin activity pag1

---
+++ app/src/main/java/com/example/winemeup/ActivityPage1.java
@@ -0,0 +1,14 @@
+package com.example.winemeup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPage1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page1);
+    }
+}
--- app/src/main/java/com/example/winemeup/MainActivity.java
+++ app/src/main/java/com/example/winemeup/MainActivity.java
@@ -1,25 +1,28 @@
 package com.example.winemeup;
 
-import
-
-import android.widget.ImageView; ...
-
-
-
-//allright
-//yes yes
-//try
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
-    Drawable drawable;
+
+    private bu button_signup;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        button_signup = (Button) findViewbyId(R.id.button_signup);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openSignUppage();
+            }
+        });
+    }
+    public void openSignUppage() {
+        Intent intent=new Intent(this,ActivitySignUp.class);
+        startActivity(intent);
     }
 }
