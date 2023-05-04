8b6925809489326dda13d7981bff7f516f230a44
maxarasta <bdnmaxime@gmail.com>
2023-05-04 12:28:22 +0200

    for bug #1: useless comment 2

--- app/src/main/java/com/example/winemeup/MainActivity.java
+++ app/src/main/java/com/example/winemeup/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.winemeup;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private bu button_signup;
+    //private bu button_signup;
 
 
 
@@ -14,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_signup = (Button) findViewbyId(R.id.button_signup);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button_signup = (Button) findViewById(R.id.button_signup);
+        button_signup.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 openSignUppage();
             }
