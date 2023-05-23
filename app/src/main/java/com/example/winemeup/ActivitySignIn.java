335abda2dca0e06fb3dc12d6ccde34dc44604a9d
IreneDagher <irene_dagher@hotmail.com>
2023-05-23 17:48:22 +0200

    Design

--- app/src/main/java/com/example/winemeup/ActivitySignIn.java
+++ app/src/main/java/com/example/winemeup/ActivitySignIn.java
@@ -6,15 +6,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.winemeup.databinding.ActivitySignInBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
@@ -37,6 +43,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -49,94 +56,98 @@ import java.util.Objects;
 public class ActivitySignIn extends AppCompatActivity {
 
 
-   EditText PersonName,PersonPassword;
 
-    Button button_signin;
-    FirebaseAuth mAuth;
+    private ActivitySignInBinding binding;
+    private ProgressDialog progressDialog;
 
+    private FirebaseAuth firebaseAuth;
+    private String email="",password="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding= ActivitySignInBinding.inflate(getLayoutInflater());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mAuth = FirebaseAuth.getInstance();
-        PersonName = findViewById(R.id.PersonName);
-        PersonPassword = findViewById(R.id.PersonPassword);
+        setContentView(binding.getRoot());
 
-        button_signin = findViewById(R.id.button_signin);
+        progressDialog= new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setMessage("Signing in");
+        progressDialog.setCanceledOnTouchOutside(false);
 
-        button_signin.setOnClickListener(new View.OnClickListener() {
-                                             @Override
-                                             public void onClick(View view) {
-                                                 if (!validateMail() | !validatePassword()) {
-
-                                                 }else{
-                                                     checkUser();
-                                                 }
-                                             }
-
-                                         }
-        );
-
-    }
-
-    public Boolean validateMail(){
-        String val=PersonName.getText().toString();
-        if(val.isEmpty()){
-            PersonName.setError("Already drunk ? Username cannot be empty");
-            return false;
-
-        }else{
-            PersonName.setError(null);
-            return true;
-        }
-    }
-
-    public Boolean validatePassword(){
-        String val=PersonPassword.getText().toString();
-        if(val.isEmpty()){
-            PersonPassword.setError("Already drunk ?Password cannot be empty");
-            return false;
-
-        }else{
-            PersonPassword.setError(null);
-            return true;
-        }
-    }
-    public void checkUser(){
-        String userMail=PersonName.getText().toString().trim();
-        String userPassword=PersonPassword.getText().toString().trim();
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
-        Query checkUserDatabase=reference.orderByChild("username").equalTo(userMail);
-        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+        //if have account,signup
+        firebaseAuth=FirebaseAuth.getInstance();
+        binding.haveAccountTv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
-                    PersonName.setError(null);
-                    String passwordFromDB=snapshot.child(userPassword).child("password").getValue(String.class);
-                    if(!Objects.equals(passwordFromDB,userPassword)){
-                        PersonName.setError(null);
-                        Intent intent=new Intent(ActivitySignIn.this,ActivityPage1.class);
-                        startActivity(intent);
-                    }else{
-                        PersonPassword.setError("Invalid credentials");
-                        PersonPassword.requestFocus();
-                    }
-                }else{
-                    PersonName.setError("User does not exist");
-                    PersonName.requestFocus();
-                }
-            }
+            public void onClick(View view) {
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            startActivity(new Intent(ActivitySignIn.this,ActivitySignUp.class));
 
             }
         });
 
+        binding.buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateData();
+            }
+
+
+        });
+
     }
 
+private void validateData(){
+        email=binding.PersonName.getText().toString().trim();
+        password=binding.PersonPassword.getText().toString().trim();
+    if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        //not valid email format
+        binding.PersonName.setError("Are you already drunk? Invalid email format");
+
+
+    } else if (TextUtils.isEmpty(password)) {
+        //no password is entered
+        binding.PersonPassword.setError("Are you already drunk? You should enter a password");
+
+    }
+    else if(password.length()<4){
+        //password less than 4 letters
+        binding.PersonName.setError("At least 4 characters! W.I.N.E");
+    }
+    else{
+        //data is okay
+        firebaseSignin();
+
+    }
+
+}
+
+    private void firebaseSignin() {
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //succesfull
+
+                        FirebaseUser  firebaseUser=firebaseAuth.getCurrentUser();
+                        String email=firebaseUser.getEmail();
+                        Toast.makeText(ActivitySignIn.this,"LoggedIn\n"+email,Toast.LENGTH_SHORT).show();
+
+                        startActivity(new Intent(ActivitySignIn.this,ActivityPage1.class));
+                        finish();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ActivitySignIn.this, ""+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
 
 
 };
--- app/src/main/java/com/example/winemeup/ActivitySignUp.java
+++ app/src/main/java/com/example/winemeup/ActivitySignUp.java
@@ -6,15 +6,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.winemeup.databinding.ActivitySignUpBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 //import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
@@ -43,44 +48,107 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class ActivitySignUp extends AppCompatActivity {
 
-    EditText PersonName2, PersonMail2, PersonPassword2;
+    private ActivitySignUpBinding binding;
+    private FirebaseAuth firebaseAuth;
 
-    Button button_signup;
-    FirebaseDatabase database;
-    DatabaseReference reference;
+    //showing the progress
+    private ProgressDialog progressDialog;
+
+
+    private String email="",password="";
+
 
     FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mAuth = FirebaseAuth.getInstance();
-        PersonName2 = findViewById(R.id.PersonName2);
-        PersonPassword2 = findViewById(R.id.PersonPassword2);
-        PersonMail2 = findViewById(R.id.PersonMail2);
-
-        button_signup = findViewById(R.id.button_signup);
-
-        button_signup.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth=FirebaseAuth.getInstance();
+        binding.textView8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name, email, password;
-                database = FirebaseDatabase.getInstance();
-                reference = database.getReference("users");
-                name = PersonName2.getText().toString();
-                email = PersonMail2.getText().toString();
-                password = PersonPassword2.getText().toString();
 
-
-                Toast.makeText(ActivitySignUp.this, "Succesfully sign up!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(ActivitySignUp.this, ActivityPage1.class);
-                startActivity(intent);
-
+                startActivity(new Intent(ActivitySignUp.this,ActivitySignIn.class));
 
             }
         });
+
+        //configure progress digalog
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("Getting the wine, please wait");
+        progressDialog.setMessage("Creating the account !");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        binding.buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateData();
+
+            }
+        });
+    }
+
+    private void validateData() {
+        //getting the data when the user writes it
+        email=binding.PersonMail2.getText().toString().trim();
+        password=binding.PersonPassword2.getText().toString().trim();
+        //now actually validating the data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //not valid email format
+            binding.PersonMail2.setError("Are you already drunk? Invalid email format");
+
+
+    } else if (TextUtils.isEmpty(password)) {
+            //no password is entered
+            binding.PersonPassword2.setError("Are you already drunk? You should enter a password");
+
+        }
+        else if(password.length()<4){
+            //password less than 4 letters
+            binding.PersonMail2.setError("At least 4 characters! W.I.N.E");
+        }
+        else{
+            //data is okay
+            firebaseSignUp();
+
+        }
+
+
+    }
+
+    private void firebaseSignUp() {
+        //show proogress
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //sign up succesfully
+                        progressDialog.dismiss();
+                        //get user info
+                        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+                        String email=firebaseUser.getEmail();
+                        Toast.makeText(ActivitySignUp.this,"Account created\n"+email,Toast.LENGTH_SHORT).show();
+                        //open the activity searching wine
+                        startActivity(new Intent(ActivitySignUp.this,ActivityPage1.class));
+                        finish();
+
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failure
+                        progressDialog.dismiss();
+                        Toast.makeText(ActivitySignUp.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 }
